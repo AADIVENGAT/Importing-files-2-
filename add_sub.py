1bc366c36582d465c0bfbb3792d7e261b6ed2035
AADIVENGAT <88031195+AADIVENGAT@users.noreply.github.com>
2022-03-05 07:11:19 +0530

    Add files via upload

---
+++ add_sub.py
@@ -0,0 +1,17 @@
+#importing files
+def add(x,y):
+    z=x+y
+    print(f"addition: ",{z})
+def sub(x,y):
+    z=x-y
+    print(f"subtraction: ",{z})
+
+if __name__  == "__main__":
+    add(1,5)
+    sub(5,2)
+
+print(__name__)        
+    
+
+
+
---
+++ march4.1.py
@@ -0,0 +1,12 @@
+#importing files
+import add_sub
+def mul(x,y):
+    z=x*y
+    print(f"mul: ",{z})
+def div(x,y):
+    z=x/y
+    print(f"div: ",{z})
+
+mul(1,5)
+div(5,2)
+print(__name__)
